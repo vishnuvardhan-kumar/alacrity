fa17777c43b6b1f8533eea52f731cf2f9fdb7f58
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-07-05 19:16:18 +0530

    Added placeholder tests

--- alacrity/tests/test_lib.py
+++ alacrity/tests/test_lib.py
@@ -1,1 +1,16 @@
 # Unittests for the lib.py functions to be placed here
+
+import unittest
+
+
+class TestParser(unittest.TestCase):
+    """ To be built """
+
+    def test_placeholder(self):
+        self.assertTrue(True)
+
+    def test_placeholder_2(self):
+        self.assertFalse(False)
+
+    def test_placeholder_3(self):
+        self.assertEqual(2+2, 4)
