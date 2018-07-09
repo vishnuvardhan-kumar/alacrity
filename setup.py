ac7f552b120dcd871a5de22d451c76474340a141
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-07-09 20:27:50 +0530

    Fixed KeyboardInterrupt handling

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file_obj:
 
 setuptools.setup(
     name="alacrity",
-    version="0.1.2",
+    version="1.0.1",
     author="Vishnuvardhan Kumar",
     author_email="vishnukumar1997@gmail.com",
     description="Quickstart your Python development with CLI package templating",
