b58fa2d57ac40e51ef5ee61359a6f673fef10105
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2020-08-08 17:01:39 +0530

    Corrected classifiers

--- setup.py
+++ setup.py
@@ -21,9 +21,10 @@ setuptools.setup(
       },
     include_package_data=True,
     classifiers=(
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
